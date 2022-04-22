f4801fc41ad9a22f10ad8ec4c45a92be2351b554
Shih yao Cheng <samsy318417@gmail.com>
2022-04-22 11:55:48 +0800

    line bot 測試

--- src/com/cqi/hr/constant/Constant.java
+++ src/com/cqi/hr/constant/Constant.java
@@ -113,7 +113,7 @@ public class Constant {
 	
 	// Developer
 	// 需用ngrok把local8080連接到ngrok.io,並在asana app 設定Redirect URLs,此區也須跟著修改
-	public final static String ASANA_REDIRECT_URL = "https://e5bb-36-230-188-94.ngrok.io/hr-manage/asanaCallback";
+	public final static String ASANA_REDIRECT_URL = "https://5b0b-118-168-34-63.ngrok.io/hr-manage/asanaCallback";
 	
 	// Production
 //	public final static String ASANA_REDIRECT_URL = "https://hr.cqiserv.com/asanaCallback";
